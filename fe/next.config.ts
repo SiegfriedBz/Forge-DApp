2bfc12f855cacf35b1e758fe5b08385f37abd6ad
siegfriedbz <siegfried.bozza@yahoo.com>
2026-05-08 10:29:53 +0200

    fix Turbopqck build errors

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	/* config options here */
-};
+	// Tells Next.js not to bundle these Node-specific libraries
+	serverExternalPackages: ['pino', 'thread-stream'],
+}
 
-export default nextConfig;
+export default nextConfig
